f46d6e2815b1d940e3442173db9debad0682289b
Deborah Arku <arkudeborah123@gmail.com>
2025-12-01 18:35:18 +0100

    Added the user, ticket and category entity

--- src/main/kotlin/com/example/Kodify/entities/Category.kt
+++ src/main/kotlin/com/example/Kodify/entities/Category.kt
@@ -1,4 +1,15 @@
 package com.example.Kodify.entities
 
-class Category {
-}
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "category")
+data class Category(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(unique = true)
+    val name: String
+)
--- src/main/kotlin/com/example/Kodify/entities/Ticket.kt
+++ src/main/kotlin/com/example/Kodify/entities/Ticket.kt
@@ -1,4 +1,60 @@
 package com.example.Kodify.entities
+import jakarta.persistence.*
+import java.time.Instant
 
-class Ticket {
+enum class TicketPriority {
+    LOW,
+    MEDIUM,
+    HIGH,
+    CRITICAL
 }
+
+enum class TicketStatus {
+    NEW,
+    ASSIGNED,
+    IN_PROGRESS,
+    PENDING,
+    RESOLVED,
+    CLOSED
+}
+
+@Entity
+@Table(name = "ticket")
+data class Ticket(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(unique = true, nullable = false)
+    val ticketNumber: String,
+
+    val title: String,
+
+    @Lob // we use this for potentially long texts, descriptions can be lengthy
+    val description: String,
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    val category: Category,
+
+    @Enumerated(EnumType.STRING)
+    val priority: TicketPriority,
+
+    @Enumerated(EnumType.STRING)
+    val status: TicketStatus = TicketStatus.NEW,
+
+    // use @ElementCollection only for lists of basic data like strings or numbers
+    @ElementCollection
+    @CollectionTable(name = "ticket_attachment", joinColumns = [JoinColumn(name = "ticket_id")])
+    @Column(name = "attachment_path")
+    val attachments: MutableSet<String> = mutableSetOf(),
+
+    val created_at: Instant = Instant.now(),
+    var updated_at: Instant = Instant.now()
+)
